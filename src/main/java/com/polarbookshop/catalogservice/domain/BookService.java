087bab874f367147df7529314bf34e1291c9b726
isuru <isurucuma@gmail.com>
2023-11-19 18:29:43 +0530

    Initial commit

---
+++ src/main/java/com/polarbookshop/catalogservice/domain/BookService.java
@@ -0,0 +1,47 @@
+package com.polarbookshop.catalogservice.domain;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
+
+    public Iterable<Book> viewBookList(){
+        return bookRepository.findAll();
+    }
+
+    public Book viewBookDetails(String isbn){
+        return bookRepository.findByIsbn(isbn)
+                .orElseThrow(() -> new BookNotFoundException(isbn));
+    }
+
+    public Book addBookToCatalog(Book book){
+        if(bookRepository.existsByIsbn(book.isbn())){
+            throw new BookAlreadyExistsException(book.isbn());
+        }
+        return bookRepository.save(book);
+    }
+
+    public void removeBookFromCatalog(String isbn){
+        bookRepository.deleteByIsbn(isbn);
+    }
+
+    public Book editBookDetails(String isbn, Book book){
+        return bookRepository.findByIsbn(isbn)
+                .map(existingBook -> {
+                    Book bookToUpdate = new Book(
+                            existingBook.isbn(),
+                            book.isbn(),
+                            book.author(),
+                            book.price()
+                    );
+                    return bookRepository.save(bookToUpdate);
+                })
+                .orElseGet(() -> addBookToCatalog(book));
+    }
+
+}
---
+++ src/test/java/com/polarbookshop/catalogservice/web/BookControllerMvcTests.java
@@ -0,0 +1,33 @@
+package com.polarbookshop.catalogservice.web;
+
+import com.polarbookshop.catalogservice.domain.BookNotFoundException;
+import com.polarbookshop.catalogservice.domain.BookService;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(BookController.class)
+public class BookControllerMvcTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    void whenGetBookNotExistingThenShouldReturn404() throws Exception{
+        String isbn = "1234567890";
+        given(bookService.viewBookDetails(isbn))
+                .willThrow(BookNotFoundException.class);
+        mockMvc
+                .perform(MockMvcRequestBuilders.get("/books/" + isbn))
+                .andExpect(status().isNotFound());
+    }
+}
